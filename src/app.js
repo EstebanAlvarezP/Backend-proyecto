f4a6719e85c01222edd4192211f31b5cca0bf5b9
Esteban <esteban.yesid2255@gmail.com>
2024-01-28 15:43:24 -0500

    primera entrega

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const fs = require('fs/promises'); // Utilizamos fs/promises para operaciones asincrónicas
+const fs = require('fs/promises'); 
 
-// Configuración para parsear el cuerpo de las solicitudes (body)
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Archivos de persistencia
+
 const productsFilePath = './productos.json';
 const cartsFilePath = './carrito.json';
 
-// Datos de ejemplo (simulando una base de datos)
+
 let productsData = [];
 let cartsData = [];
 
-// Función para cargar datos desde archivos
+
 async function loadFromFile() {
   try {
     const productsFileContent = await fs.readFile(productsFilePath, 'utf-8');
@@ -24,12 +24,12 @@ async function loadFromFile() {
     productsData = JSON.parse(productsFileContent);
     cartsData = JSON.parse(cartsFileContent);
   } catch (error) {
-    // Si hay un error al leer los archivos, no se carga nada (podrías manejarlo de manera diferente según tus necesidades)
+    
     console.error('Error al cargar datos desde archivos:', error.message);
   }
 }
 
-// Función para guardar datos en archivos
+
 async function saveToFile() {
   try {
     await fs.writeFile(productsFilePath, JSON.stringify(productsData, null, 2), 'utf-8');
@@ -39,12 +39,12 @@ async function saveToFile() {
   }
 }
 
-// Grupo de rutas "products"
+
 const productsRouter = express.Router();
 
-// Ruta raíz GET /api/products
+
 productsRouter.get('/', (req, res) => {
-  // Aplicar la limitación si se proporciona el parámetro ?limit
+
   const limit = req.query.limit;
   const limitedProducts = limit ? productsData.slice(0, parseInt(limit, 10)) : productsData;
   res.json(limitedProducts);
@@ -61,18 +61,18 @@ productsRouter.get('/:pid', (req, res) => {
     }
   });
 
-// Ruta DELETE /api/products/:pid
+
 productsRouter.delete('/:pid', (req, res) => {
   const productId = parseInt(req.params.pid, 10);
 
-  // Filtrar la lista de productos para excluir el producto con el ID especificado
+  
   const updatedProducts = productsData.filter(p => p.id !== productId);
 
   if (updatedProducts.length < productsData.length) {
-    // Si se eliminó al menos un producto, actualiza la lista y responde con éxito
+   
     productsData = updatedProducts;
 
-    // Guardar datos en el archivo después de la eliminación
+   
     saveToFile();
 
     res.json({ success: true });
@@ -83,10 +83,10 @@ productsRouter.delete('/:pid', (req, res) => {
 
 app.use('/api/products', productsRouter);
 
-// Grupo de rutas "carts"
+
 const cartsRouter = express.Router();
 
-// Ruta raíz GET /api/carts
+
 cartsRouter.get('/:cid', (req, res) => {
     const cartId = req.params.cid;
     const cart = cartsData.find(c => c.id === cartId);
@@ -98,28 +98,28 @@ cartsRouter.get('/:cid', (req, res) => {
     }
   });
 
-// Ruta POST /api/carts/:cid/product/:pid
+
 cartsRouter.post('/:cid/product/:pid', (req, res) => {
   const cartId = req.params.cid;
   const productId = parseInt(req.params.pid, 10);
   const { quantity } = req.body;
 
-  // Buscar el carrito por su ID
+  
   const cart = cartsData.find(c => c.id === cartId);
 
   if (cart) {
-    // Verificar si el producto ya existe en el carrito
+    
     const existingProductIndex = cart.products.findIndex(p => p.product === productId);
 
     if (existingProductIndex !== -1) {
-      // Si el producto ya está en el carrito, incrementar la cantidad
+      
       cart.products[existingProductIndex].quantity += quantity || 1;
     } else {
-      // Si el producto no está en el carrito, agregarlo
+      
       cart.products.push({ product: productId, quantity: quantity || 1 });
     }
 
-    // Guardar datos en el archivo después de la actualización del carrito
+    
     saveToFile();
 
     res.json(cart.products);
@@ -130,10 +130,10 @@ cartsRouter.post('/:cid/product/:pid', (req, res) => {
 
 app.use('/api/carts', cartsRouter);
 
-// Configuración del puerto
+
 const PORT = 8080;
 
-// Cargar datos desde archivos al iniciar la aplicación
+
 loadFromFile();
 
 app.listen(PORT, () => {
